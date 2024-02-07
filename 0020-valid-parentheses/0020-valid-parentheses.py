f7b19c051ab55bee7818a44011dba77eab44fd29
Sourabh Chaudhari <38533890+SourabhMChaudhari@users.noreply.github.com>
2024-02-07 17:16:51 -0500

    Time: 21 ms (99.69%), Space: 16.6 MB (59.60%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,34 +1,35 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         
-#         stack = []
-        
-#         dict_sign = {'(' : ')', '{': '}', '[': ']'}
-        
-#         if s == "":
-#             return True
-#         for char in s :
-            
-#             if char in dict_sign.keys():
-#                 stack.append(char)
-            
-#             if char in dict_sign.values():
-                
-#                 if stack == [] or dict_sign[char] != stack.pop():
-#                     return False
-        
-#         return stack == []
-        
         stack = []
-        dict = {"]":"[", "}":"{", ")":"("}
-        for char in s:
-            if char in dict.values():
+        
+        dict_sign = {'(' : ')', '{': '}', '[': ']'}
+        
+        print(dict_sign.values())
+        if s == "":
+            return True
+        for char in s :
+            
+            if char in dict_sign.keys():
                 stack.append(char)
-            elif char in dict.keys():
-                if stack == [] or dict[char] != stack.pop():
+            
+            elif char in dict_sign.values():
+                
+                if stack == [] or dict_sign[stack.pop()] != char :
                     return False
-            else:
-                return False
+        
         return stack == []
+        
+        # stack = []
+        # dict = {"]":"[", "}":"{", ")":"("}
+        # for char in s:
+        #     if char in dict.values():
+        #         stack.append(char)
+        #     elif char in dict.keys():
+        #         if stack == [] or dict[char] != stack.pop():
+        #             return False
+        #     else:
+        #         return False
+        # return stack == []
             
             
