bc5f3b66df339b66798c7f456e3b5223e2443bd9
Sourabh Chaudhari <38533890+SourabhMChaudhari@users.noreply.github.com>
2024-02-16 19:25:40 -0500

    Time: 98 ms (29.26%), Space: 21.1 MB (77.89%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,25 +1,22 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        hs = {}
-        frq = {}
-        for i in xrange(0, len(nums)):
-            if nums[i] not in hs:
-                hs[nums[i]] = 1
-            else:
-                hs[nums[i]] += 1
+import heapq
+from typing import List
 
-        for z,v in hs.iteritems():
-            if v not in frq:
-                frq[v] = [z]
-            else:
-                frq[v].append(z)
-        
-        arr = []
-        
-        for x in xrange(len(nums), 0, -1):
-            if x in frq:
-                
-                for i in frq[x]:
-                    arr.append(i)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]: 
+        frequency_map = {}
+        heap = []
 
-        return [arr[x] for x in xrange(0, k)]
+        # Count the frequency of each number
+        for num in nums:
+            frequency_map[num] = frequency_map.get(num, 0) + 1
+
+        # Push elements into the heap
+        for num, freq in frequency_map.items():
+            heapq.heappush(heap, (-freq, num))
+
+        # Pop k elements from the heap
+        top_k = []
+        for _ in range(k):
+            top_k.append(heapq.heappop(heap)[1])
+
+        return top_k
