0b13d50e798626bdd00e7fe060eb6171e49d3ea6
Sourabh Chaudhari <38533890+SourabhMChaudhari@users.noreply.github.com>
2024-05-31 11:07:45 -0400

    Time: 34 ms (74.68%), Space: 16.3 MB (97.86%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,13 +1,14 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        # Convert the list of digits to a string
-        number_str = ''.join(map(str, digits))
-        # Convert the string to an integer
-        number = int(number_str)
-        # Add 1 to the number
-        incremented_number = number + 1
-        # Convert the incremented number back to a string
-        incremented_number_str = str(incremented_number)
-        # Convert the string to a list of digits
-        result_list = [int(digit) for digit in incremented_number_str]
-        return result_list
+              
+            digit = ''.join(map(str, digits))
+            
+            int_digit = int(digit)
+            
+            incremental_value = int_digit + 1
+            
+            str_digit = str(incremental_value)
+            
+            digit_list = [int(value) for value in str_digit]
+            
+            return digit_list
