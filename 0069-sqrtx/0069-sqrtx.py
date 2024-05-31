7790decbee3f6983b2bd15c4d8eaa945804d3e10
Sourabh Chaudhari <38533890+SourabhMChaudhari@users.noreply.github.com>
2024-05-31 13:17:13 -0400

    Time: 47 ms (29.30%), Space: 16.5 MB (37.98%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -15,10 +15,17 @@ class Solution:
         if x < 2:
             return x
         
-        for num in range(2, x // 2 + 1):
-            if num * num == x:
-                return num
-            elif num * num > x:
-                return num - 1
+        left, right = 2, x // 2
         
-        return x // 2
+        while left <= right:
+            mid = (left + right) // 2
+            num = mid * mid
+            
+            if num == x:
+                return mid
+            elif num < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return right
