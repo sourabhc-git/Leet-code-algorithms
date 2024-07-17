7bc85dae4e5988a69ba80a51ce36e8e58a9ad141
Sourabh Chaudhari <38533890+SourabhMChaudhari@users.noreply.github.com>
2024-07-17 17:18:09 -0400

    Time: 229 ms (82.14%), Space: 19.1 MB (47.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,9 +1,7 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
         counter, value  = 0, 0
         rows, cols = len(grid), len(grid[0])
-
         def checkonsides(grid, i, j):
             if i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j] != '1':
                 return
@@ -12,11 +10,9 @@ class Solution:
             checkonsides(grid, i, j + 1)
             checkonsides(grid, i - 1, j)
             checkonsides(grid, i, j - 1)
-
         for i in range(0, rows):
             for j in range(0, cols):
                   if grid[i][j] == '1':
                     counter = counter + 1
                     checkonsides(grid, i, j)
-        
         return counter
