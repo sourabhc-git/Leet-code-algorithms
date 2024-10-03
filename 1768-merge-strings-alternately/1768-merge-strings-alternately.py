c21e59765f9089e48cf9600c670587e2c5ef545f
Sourabh Chaudhari <38533890+sourabhc-git@users.noreply.github.com>
2024-10-02 21:35:21 -0400

    Time: 38 ms (35.56%), Space: 16.6 MB (35.79%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -0,0 +1,23 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        
+        n = len(word1) if len(word1) < len(word2) else len(word2)
+            
+        final_answer = []
+        word1 = list(word1)
+        word2 = list(word2)
+        for i in range(0, n):
+            
+            final_answer.append(word1[i])
+            final_answer.append(word2[i])
+        
+        if len(word1) == len(word2):
+            return "".join(final_answer)
+
+        if n == len(word1):
+            final_answer.extend(word2[n:])
+        else:
+            final_answer.extend(word1[n:])
+            
+        return "".join(final_answer)  
+            
