fac04dc68f3f94b84bbf6c267704db399cc39a5e
Sourabh Chaudhari <38533890+sourabhc-git@users.noreply.github.com>
2024-11-11 17:16:25 -0500

    Time: 7 ms (29.16%), Space: 17.6 MB (70.48%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,16 +1,30 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
 
-        dict1 = {}
-        for i  in range(0, len(numbers)):
-
-            complement  = target - numbers[i]
-            if complement not in dict1:
-                dict1[numbers[i]] = i
-            else:
-                return [dict1[complement]+1, i+1]
+        #dict1 = {}
+        #for i  in range(0, len(numbers)):
+        #
+        #    complement  = target - numbers[i]
+        #    if complement not in dict1:
+        #        dict1[numbers[i]] = i
+        #    else:
+        #        return [dict1[complement]+1, i+1]
             
-
+        
+        start = 0
+        
+        end = len(numbers) - 1
+        
+        while start < end:
+            
+            sum_ = numbers[start] + numbers[end]
+            
+            if sum_ == target:
+                return [start+1, end+1]
+            elif sum_ > target:
+                end -=1
+            elif sum_ < target:
+                start +=1
 
 
         
