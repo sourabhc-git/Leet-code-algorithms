e73332a66a1904774a5524e47ed1b2bea87b35bf
Sourabh Chaudhari <38533890+sourabhc-git@users.noreply.github.com>
2024-12-10 17:11:18 -0500

    Time: 239 ms (10.33%), Space: 17.7 MB (6.91%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,44 +1,19 @@
-# class Solution:
-#     def characterReplacement(self, s: str, k: int) -> int:
-        
-#         l = 0
-        
-#         max_num = 0
-        
-#         count  = {}
-        
-        
-#         for i in range(0, len(s)):
-            
-#             count[s[i]] = 1 + count.get(s[i], 0)  #here we can get the character count if its present or else 0
-            
-#             max_num = max(max_num, count[s[i]])
-            
-#             if (i - l + 1 - max_num) > k :
-                
-#                 count[s[i]] -=1
-#                 l +=1
-            
-        
-#         return (i - l + 1)
-
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
         
         l = 0
-        maxf = 0
+        value = 0
+        counts = [0] * 26
         for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)
-            maxf = max(maxf, count[s[r]])
-
-            if (r - l + 1) - maxf > k:
-                count[s[l]] -= 1
-                l += 1
-
-        return (r - l + 1)
-
             
+            counts[ord(s[r]) - 65] +=1
+            while ((r - l + 1) - max(counts)) > k:
+
+                counts[ord(s[l]) - 65] -=1
+                l +=1
             
-    
+            value = max(value, (r-l+1))
         
+        return value
+
+
